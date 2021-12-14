27e60d5455ebef33cc98b4b06c1f2ec96ca5f487
Estela Paulino <estelapaulino@campos.rj.gov.br>
2021-12-14 11:40:26 -0300

    remoção do diretório .idea e criação dos modelos e admin da biblioteca

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from base.models import Biblioteca, Titulo, Exemplar
 
-# Register your models here.
+@admin.register(Biblioteca)
+class BibliotecaAdmin(admin.ModelAdmin):
+    list_display = 'nome',
+
+
+@admin.register(Titulo)
+class TituloAdmin(admin.ModelAdmin):
+    list_display = 'nome', 'categoria', 'imagem',
+    ordering = 'nome',
+    list_filter = 'categoria',
+
+
+@admin.register(Exemplar)
+class ExemplarAdmin(admin.ModelAdmin):
+    list_display = 'data_aquisicao', 'titulo', 'ativo',
+    ordering = 'titulo',
+
--- base/models.py
+++ base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Biblioteca(models.Model):
+    nome = models.CharField(verbose_name='Nome', max_length=50)
+
+    class Meta:
+        verbose_name = 'Biblioteca'
+        verbose_name_plural = 'Bibliotecas'
+
+    def __str__(self):
+        return self.nome
+
+class Titulo(models.Model):
+    nome = models.CharField(verbose_name='Nome', max_length=50)
+    categoria = models.CharField(verbose_name='Categoria', choices=[['LIVRO', 'Livro'], ['REVISTA', 'Revista']], max_length=50)
+    imagem = models.ImageField(verbose_name='Imagem', upload_to='titulos')
+
+    class Meta:
+        verbose_name = 'Título'
+        verbose_name_plural = 'Títulos'
+
+    def __str__(self):
+        return self.nome
+
+
+class Exemplar(models.Model):
+    data_aquisicao = models.DateField(verbose_name='Data de aquisição')
+    titulo = models.ForeignKey(Titulo, verbose_name='Título', on_delete=models.CASCADE)
+    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+
+    class Meta:
+        verbose_name = 'Exemplar'
+        verbose_name_plural = 'Exemplares'
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.titulo)
